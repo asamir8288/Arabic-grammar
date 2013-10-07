3d1b02440d068e11c57dae590f5c9973c1916ffe
Ahmed Samir <ahmed.samir@basharsoft.com>
2013-10-07 22:41:59 +0200

    commiting

--- layout/js/choose-each-answer-exam.js
+++ layout/js/choose-each-answer-exam.js
@@ -2,7 +2,7 @@ $(document).ready( function(){
     
     $(".normal-span-answer").live("click", function(){
         var highlighted = $(".selected-span-answer");
-        highlighted.removeClass("selected-span-answer");
+//        highlighted.removeClass("selected-span-answer");
         highlighted.addClass("normal-span-answer");
 
         $(this).removeClass("normal-span-answer");
